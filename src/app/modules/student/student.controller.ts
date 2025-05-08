8c89ee9b651ce6bcbca27d10643c3275566ef4a0
ashraful-rahat <spinball61@gamil.com>
2025-05-08 23:22:39 +0600

    few thing added

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -25,6 +25,31 @@ const getSingleStudent = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+// const updateStudent = catchAsync(async (req, res) => {
+//   const { id } = req.params;
+//   const { student } = req.body;
+//   const result = await StudentServices.updateStudentIntoDB(id, student);
+
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: 'Student is updated successfully',
+//     data: result,
+//   });
+// });
+
+// const deleteStudent = catchAsync(async (req, res) => {
+//   const { id } = req.params;
+//   const result = await StudentServices.deleteStudentFromDB(id);
+
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: 'Student is deleted successfully',
+//     data: result,
+//   });
+// });
+
 export const StudentControllers = {
   getAllStudents,
   getSingleStudent,
